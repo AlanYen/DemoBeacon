a7b7e2a99861de02eb7732078311864299a2133f
AlanYen <alanyen.mobile@gmail.com>
2015-09-10 15:13:02 +0800

    Initial Commit

---
+++ DemoBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoBeacon
+//
+//  Created by AlanYen on 2015/9/10.
+//  Copyright (c) 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoBeacon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoBeacon
+//
+//  Created by AlanYen on 2015/9/10.
+//  Copyright (c) 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
